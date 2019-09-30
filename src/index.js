f48c043b6cc67faeef5d3ab419b393b3cd5596df
Siarhei Liavontsyeu <leontevsi@gmail.com>
2019-09-30 18:33:25 +0300

    refactor: change loop

--- src/index.js
+++ src/index.js
@@ -1,43 +1,41 @@
 module.exports = function multiply(one, two) {
     //split string and reverse array
-    let arrOne = one.split('').reverse();
-    let arrTwo = two.split('').reverse();
-    // console.log(arrOne);
-    // console.log(arrTwo);
+    let arrOne = one.split('').reverse(),
+        arrTwo = two.split('').reverse();
+    // console.log('arrOne', arrOne);
+    // console.log('arrTwo', arrTwo);
 
+    /*
+                55
+   *            55
+   ---------------
+               275
+            + 275
+            ------
+             3025      
+    */
     let arrMult = [];
-
-    //mult all elements in arrOne with all elements arrTwo 
-    for (let i = 0; i < arrOne.length; i++) {
-
-        for (let j = 0; j < arrTwo.length; j++) {
-            let mult = arrOne[i] * arrTwo[j];
-
+    arrOne.forEach((elem1, i) => {
+        arrTwo.forEach((elem2, j) => {
+            let mult = elem1 * elem2;
             if (arrMult[i + j]) { // if exist then sum
                 arrMult[i + j] += mult;
-            } else { 
+            } else {
                 arrMult[i + j] = mult;
             }
-
-        }
-    }
+        });
+    });
     // console.log('arrMult', arrMult);
 
-    //check elements, if element is composite then move a remainder of the division to  the next position
-    for (let i = 0; i < arrMult.length; i++) {
-        let rem = Math.floor(arrMult[i] / 10); // remainder of the devision
-
-        arrMult[i] = arrMult[i] % 10; // get and write simple number
-
+    arrMult.forEach((elem, i) => {
+        let rem = Math.floor(elem / 10); // remainder of the devision
+        arrMult[i] = elem % 10; // get and write simple number
         if (arrMult[i + 1]) {
             arrMult[i + 1] += rem;
         } else if (rem != 0) {
             arrMult[i + 1] = rem;
         }
-        // console.log('rem', rem);
-
-    }
-
+    });
     // console.log('arrMult', arrMult);
 
     let res = arrMult.reverse().join('');
